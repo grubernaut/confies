ca725105f0bd53a2d82db2fc191ae3529fc1b76f
Jake Champlin <jake.champlin.27@gmail.com>
2014-11-23 04:38:32 -0500

    Version Bump
    BAMPS

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jake.champlin.27@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures confies'
 long_description 'Installs/Configures confies'
-version          '0.1.0'
+version          '0.1.2'
 
 depends 'pacman'
 depends 'oh-my-zsh', '~> 0.4.3'
